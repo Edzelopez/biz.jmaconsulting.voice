481c9e82263e083672d595e1efb4dc4004005b95
Eftakhairul Islam <eftakhairul@gmail.com>
2014-06-23 12:04:26 -0400

    Install SQL and Uninstall SQL file are updated

--- SQL/voice_uninstall.sql
+++ SQL/voice_uninstall.sql
@@ -1,6 +1,13 @@
-DROP TABLE `civicrm_voice_spool`
-DROP TABLE `civicrm_voice_response`
-DROP TABLE `civicrm_voice_job`
-DROP TABLE `civicrm_voice_recipients`
-DROP TABLE `civicrm_voice_group`
-DROP TABLE `civicrm_voice`
+-- /*******************************************************
+-- *
+-- * voice Extension
+-- *
+-- * Eftakhairul Islam <eftakhairul@gmail.com>
+-- *******************************************************/
+
+DROP TABLE IF EXISTS `civicrm_voice_braodcast_group`;
+DROP TABLE IF EXISTS `civicrm_voice_broadcast`;
+DROP TABLE IF EXISTS `civicrm_voice_broadcast_call`;
+DROP TABLE IF EXISTS `civicrm_voice_broadcast_recipients`;
+DROP TABLE IF EXISTS `civicrm_voice_broadcast_response`;
+DROP TABLE IF EXISTS `civicrm_voice_broadcast_spool`;
