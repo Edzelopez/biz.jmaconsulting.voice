2c50f7eb84f3f8c36f996e877970c0f2ec0e5dd0
Eftakhairul Islam <eftakhairul@gmail.com>
2014-06-24 15:33:31 -0400

    Setter and getter functions are added to VoiceBroadcast

--- CRM/Voice/Entities/CivicrmVoiceBroadcast.php
+++ CRM/Voice/Entities/CivicrmVoiceBroadcast.php
@@ -113,4 +113,232 @@ class CivicrmVoiceBroadcast
     private $createdAt;
 
 
+    /**
+     * @param int $campaignId
+     */
+    public function setCampaignId($campaignId)
+    {
+        $this->campaignId = $campaignId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCampaignId()
+    {
+        return $this->campaignId;
+    }
+
+    /**
+     * @param int $contactId
+     */
+    public function setContactId($contactId)
+    {
+        $this->contactId = $contactId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getContactId()
+    {
+        return $this->contactId;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param int $domainId
+     */
+    public function setDomainId($domainId)
+    {
+        $this->domainId = $domainId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDomainId()
+    {
+        return $this->domainId;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param boolean $isPrimary
+     */
+    public function setIsPrimary($isPrimary)
+    {
+        $this->isPrimary = $isPrimary;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsPrimary()
+    {
+        return $this->isPrimary;
+    }
+
+    /**
+     * @param boolean $isTrackCallCost
+     */
+    public function setIsTrackCallCost($isTrackCallCost)
+    {
+        $this->isTrackCallCost = $isTrackCallCost;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsTrackCallCost()
+    {
+        return $this->isTrackCallCost;
+    }
+
+    /**
+     * @param boolean $isTrackCallDisposition
+     */
+    public function setIsTrackCallDisposition($isTrackCallDisposition)
+    {
+        $this->isTrackCallDisposition = $isTrackCallDisposition;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsTrackCallDisposition()
+    {
+        return $this->isTrackCallDisposition;
+    }
+
+    /**
+     * @param boolean $isTrackCallDuration
+     */
+    public function setIsTrackCallDuration($isTrackCallDuration)
+    {
+        $this->isTrackCallDuration = $isTrackCallDuration;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsTrackCallDuration()
+    {
+        return $this->isTrackCallDuration;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param int $phoneId
+     */
+    public function setPhoneId($phoneId)
+    {
+        $this->phoneId = $phoneId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPhoneId()
+    {
+        return $this->phoneId;
+    }
+
+    /**
+     * @param int $phoneLocation
+     */
+    public function setPhoneLocation($phoneLocation)
+    {
+        $this->phoneLocation = $phoneLocation;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPhoneLocation()
+    {
+        return $this->phoneLocation;
+    }
+
+    /**
+     * @param int $phoneType
+     */
+    public function setPhoneType($phoneType)
+    {
+        $this->phoneType = $phoneType;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPhoneType()
+    {
+        return $this->phoneType;
+    }
+
+    /**
+     * @param string $voiceMessageFile
+     */
+    public function setVoiceMessageFile($voiceMessageFile)
+    {
+        $this->voiceMessageFile = $voiceMessageFile;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVoiceMessageFile()
+    {
+        return $this->voiceMessageFile;
+    }
 }
