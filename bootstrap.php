77bfce01a7bc37865c32fd6c1b430d1e769904bb
Eftakhairul Islam <eftakhairul@gmail.com>
2014-06-24 14:58:51 -0400

    Doctrine anotation driver is updated

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,9 @@ require_once  __DIR__ . '/vendor/autoload.php';
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $paths = array(__DIR__ . '/CRM/Voice/Entities');
 $isDevMode = true;
@@ -17,7 +20,13 @@ $dbParams = array(
     'dbname'   => 'test_civicrm',
 );
 
-$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$config = Setup::createConfiguration($isDevMode);
+$driver = new AnnotationDriver(new AnnotationReader(), $paths);
+
+// registering noop annotation autoloader - allow all annotations by default
+AnnotationRegistry::registerLoader('class_exists');
+$config->setMetadataDriverImpl($driver);
+
 $entityManager = EntityManager::create($dbParams, $config);
 
 $conn = $entityManager->getConnection();
