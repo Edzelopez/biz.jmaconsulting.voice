e3296a1ea71a8572a9770a5b67caf8e05f779da3
Eftakhairul Islam <eftakhairul@gmail.com>
2014-06-24 15:42:10 -0400

    Setter and getter functions are added to VoiceBroadResponse

--- CRM/Voice/Entities/CivicrmVoiceBroadcastResponse.php
+++ CRM/Voice/Entities/CivicrmVoiceBroadcastResponse.php
@@ -70,5 +70,130 @@ class CivicrmVoiceBroadcastResponse
      */
     private $createdAt;
 
+    /**
+     * @param string $cost
+     */
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+        return $this;
+    }
 
+    /**
+     * @return string
+     */
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param string $disposition
+     */
+    public function setDisposition($disposition)
+    {
+        $this->disposition = $disposition;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDisposition()
+    {
+        return $this->disposition;
+    }
+
+    /**
+     * @param string $duration
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $jobId
+     */
+    public function setJobId($jobId)
+    {
+        $this->jobId = $jobId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJobId()
+    {
+        return $this->jobId;
+    }
+
+    /**
+     * @param int $phoneId
+     */
+    public function setPhoneId($phoneId)
+    {
+        $this->phoneId = $phoneId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPhoneId()
+    {
+        return $this->phoneId;
+    }
+
+    /**
+     * @param int $contactId
+     */
+    public function setContactId($contactId)
+    {
+        $this->contactId = $contactId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getContactId()
+    {
+        return $this->contactId;
+    }
 }
