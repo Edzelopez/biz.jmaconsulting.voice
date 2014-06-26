48bce5d92138fc5f88883758e3fbc4f89f6a21d1
Eftakhairul Islam <eftakhairul@gmail.com>
2014-06-26 12:00:57 -0400

    CRM_VoiceBroadcast_BAO_VoiceBroadcast is created for process corn job

---
+++ CRM/VoiceBroadcast/BAO/CRM_VoiceBroadcast_BAO_VoiceBroadcsat.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eftakhairul
+ * Date: 6/26/14
+ * Time: 11:48 AM
+ */
+
+namespace CRM\VoiceBroadcast\BAO;
+
+
+class CRM_VoiceBroadcast_BAO_VoiceBroadcsat
+{
+    static function processQueue($mode = NULL)
+    {
+        //do dome work
+        return TRUE;
+    }
+} 
