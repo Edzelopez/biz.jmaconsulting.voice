ecea7b74b8b256beb0ad2febcecc1f0a8939792a
Eftakhairul Islam <eftakhairul@gmail.com>
2014-06-26 12:57:01 -0400

    procee_voicebroadcast is invoked in AP3

--- api/v3/Job/ProcessVoicebroadcast.php
+++ api/v3/Job/ProcessVoicebroadcast.php
@@ -23,16 +23,7 @@ function _civicrm_api3_job_processvoicebroadcast_spec(&$spec) {
  */
 function civicrm_api3_job_processvoicebroadcast($params) {
   if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array( // OK, return several data rows
-      12 => array('id' => 12, 'name' => 'Twelve'),
-      34 => array('id' => 34, 'name' => 'Thirty four'),
-      56 => array('id' => 56, 'name' => 'Fifty six'),
-    );
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
-
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
+    \CRM\VoiceBroadcast\BAO\CRM_VoiceBroadcast_BAO_VoiceBroadcast::processQueue();
   } else {
     throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
   }
