db58beddaf19c18ca797e63cdd6e90c54968f0b5
Eftakhairul Islam <eftakhairul@gmail.com>
2014-06-29 15:25:10 -0400

    unnecessary functions are removed

--- api/v3/Job/ProcessVoicebroadcast.php
+++ api/v3/Job/ProcessVoicebroadcast.php
@@ -1,18 +1,6 @@
 <?php
 
 /**
- * Job.ProcessVoicebroadcast API specification (optional)
- * This is used for documentation and validation.
- *
- * @param array $spec description of fields supported by this API call
- * @return void
- * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
- */
-function _civicrm_api3_job_processvoicebroadcast_spec(&$spec) {
-  $spec['magicword']['api.required'] = 1;
-}
-
-/**
  * Job.ProcessVoicebroadcast API
  *
  * @param array $params
@@ -22,10 +10,6 @@ function _civicrm_api3_job_processvoicebroadcast_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_job_processvoicebroadcast($params) {
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
     \CRM\VoiceBroadcast\BAO\CRM_VoiceBroadcast_BAO_VoiceBroadcast::processQueue();
-  } else {
-    throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
-  }
 }
 
