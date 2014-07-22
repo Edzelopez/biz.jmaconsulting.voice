2494065b479857f87f09fa0994926d1b4515c18a
Eftakhairul Islam <eftakhairul@gmail.com>
2014-07-22 09:13:15 -0400

    State machine is updated

--- CRM/VoiceBroadcast/StateMachine/Send.php
+++ CRM/VoiceBroadcast/StateMachine/Send.php
@@ -15,25 +15,26 @@ class CRM_VoiceBroadcast_StateMachine_Send extends CRM_Core_StateMachine
     {
         parent::__construct($controller, $action);
 
-        $this->_pages = array('CRM_VoiceBroadcast_Form_Group'       => NULL,
-                              'CRM_VoiceBroadcast_Form_Settings'    => NULL,
-                              'CRM_VoiceBroadcast_Form_Upload'      => NULL,
-                              'CRM_VoiceBroadcast_Form_Test'        => NULL,
+        $this->_pages = array('CRM_VoiceBroadcast_Form_Group'           => NULL,
+                              'CRM_VoiceBroadcast_Form_Settings'        => NULL,
+                              'CRM_VoiceBroadcast_Form_Upload'          => NULL,
+                              'CRM_VoiceBroadcast_Form_Test'            => NULL,
+                              'CRM_VoiceBroadcast_Form_Schedule'        => NULL,
                              );
 
-        if (CRM_Mailing_Info::workflowEnabled()) {
-
-//          if (CRM_Core_Permission::check('schedule mailings')) {
-//            $this->_pages['CRM_Mailing_Form_Schedule'] = NULL;
-//          }
+//        if (CRM_Mailing_Info::workflowEnabled()) {
 //
-//          if (CRM_Core_Permission::check('approve mailings')) {
-//            $this->_pages['CRM_Mailing_Form_Approve'] = NULL;
-//          }
-//
-//        } else {
-          $this->_pages['CRM_Mailing_Form_Schedule'] = NULL;
-        }
+////          if (CRM_Core_Permission::check('schedule mailings')) {
+////            $this->_pages['CRM_Mailing_Form_Schedule'] = NULL;
+////          }
+////
+////          if (CRM_Core_Permission::check('approve mailings')) {
+////            $this->_pages['CRM_Mailing_Form_Approve'] = NULL;
+////          }
+////
+////        } else {
+//          $this->_pages['CRM_Mailing_Form_Schedule'] = NULL;
+//        }
 
 
         $this->addSequentialPages($this->_pages, $action);
